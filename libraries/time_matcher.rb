b5515489a40f671b2b151122d18434650c7e3d47
Caleb Severn <calsev@gmail.com>
2017-08-14 14:17:28 -0700

    Upgraded deprecated define_runner_method->define_matcher

--- libraries/time_matcher.rb
+++ libraries/time_matcher.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 if defined?(ChefSpec)
-  ChefSpec::Runner.define_runner_method(:time_client)
+  ChefSpec.define_matcher(:time_client)
 
   def set_server_name_time_client(resource)
     ChefSpec::Matchers::ResourceMatcher.new(:time_client, :set_server_name, resource)
   end
 
-  ChefSpec::Runner.define_runner_method(:time_zone)
+  ChefSpec.define_matcher(:time_zone)
 
   def set_zone_time_zone(resource)
     ChefSpec::Matchers::ResourceMatcher.new(:time_zone, :set_zone, resource)
