b7188f9718abae689c45f6ea6566cf2a51e81a24
calsev <calsev@gmail.com>
2018-07-12 13:34:20 -0800

    Updated Metadata and added Changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog for Windows DNS Cookbook
+
+## 1.1.0
+
+* Upgraded for Chef 14
+* Added support for Windows 2008
+
+## 1.0.0
+
+* Initial public release
--- metadata.rb
+++ metadata.rb
@@ -9,11 +9,11 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.alaska.edu/oit-cookbooks/win_time/issues' if respond_to?(:issues_url)
 source_url 'https://github.alaska.edu/oit-cookbooks/win_time' if respond_to?(:source_url)
 
-version '1.0.0'
+version '1.1.0'
 
-supports 'windows', '>= 10.0' # Windows 10 or Server 2016, see https://en.wikipedia.org/wiki/List_of_Microsoft_Windows_versions
+# Windows 2008 requires WMF updates
+supports 'windows', '>= 6.1' # Windows Server 2008R2 or 7, see https://en.wikipedia.org/wiki/List_of_Microsoft_Windows_versions
 
-chef_version '>= 13.1.0' if respond_to?(:chef_version)
-ohai_version '>= 13.1.0' if respond_to?(:ohai_version)
+chef_version '>= 13.0.0' if respond_to?(:chef_version)
 
-depends 'windows', '>= 3.1.1'
+depends 'windows'
