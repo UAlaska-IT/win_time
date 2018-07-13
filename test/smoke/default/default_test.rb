e0c40345f1788e81aca724f23995483014277513
calsev <calsev@gmail.com>
2018-07-13 14:50:39 -0800

    Hardcoded test cases

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,38 +1,21 @@
 # frozen_string_literal: true
 
-tcb = 'win_time'
-
 # Validate time server is reachable
-describe host(json('C:/chef/cache/chef_node.json').params['default'][tcb]['time_server_url']) do
-  let(:node) { json('C:/chef/cache/chef_node.json').params }
-  before do
-    skip unless node['default'][tcb]['set_time_server']
-  end
-
+describe host('ntp.alaska.edu') do
   it { should be_resolvable }
   it { should be_reachable }
 end
 
 # Validate time server url
 describe powershell('w32tm /query /source') do
-  let(:node) { json('C:/chef/cache/chef_node.json').params }
-  before do
-    skip unless node['default'][tcb]['set_time_server']
-  end
-
   its('exit_status') { should eq 0 }
   its('stderr') { should eq '' }
-  its('stdout') { should match Regexp.new(node['default'][tcb]['time_server_url']) }
+  its('stdout') { should match Regexp.new('ntp.alaska.edu') }
 end
 
 # Validate time zone name
 describe powershell('Get-TimeZone | select Id') do
-  let(:node) { json('C:/chef/cache/chef_node.json').params }
-  before do
-    skip unless node['default'][tcb]['set_time_zone']
-  end
-
   its('exit_status') { should eq 0 }
   its('stderr') { should eq '' }
-  its('stdout') { should match Regexp.new(node['default'][tcb]['time_zone']) }
+  its('stdout') { should match Regexp.new('Alaskan Standard Time') }
 end
